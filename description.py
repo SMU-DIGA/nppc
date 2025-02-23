8de67671aaa315634b2e4383b575c589d47efc04
rainwangphy <rainwangphy@gmail.com>
2025-02-24 01:39:20 +0800

    minor

--- description.py
+++
@@ -1,5 +0,0 @@
-def three_sat():
-    name = "3-Satisfiability (3-SAT)"
-    description = """Input: A set of m clauses - C1 ,C2,...,Cm - over a set of n Boolean valued variables Xn=<x1,x2,...,xn>, such that each clause depends on exactly three distinct variables from Xn. A clause being a Boolean expression of the form yi+yj+yk where each y is of the form x or -x (i.e. negation of x) with x being some variable in Xn. For example if n=4 and m=3 a possible instance could be the (set of) Boolean expressions: C1=(x1+(-x2)+(-x3)); C2=(x2+x3+(-x4)); C3=((-x1)+x3+x4);
-Question: Can each variable xi of Xn be assigned a Boolean value alphai in such a way that every clause evaluates to the Boolean result true under the assignment < xi:=alphai:1<=i<=n>? """
-    return name, description
