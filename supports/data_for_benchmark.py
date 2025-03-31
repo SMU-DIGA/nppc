60946517fec3cb7abc6498a5dd8fe84acb556d87
rainwangphy <rainwangphy@gmail.com>
2025-03-31 14:13:35 +0800

    supports

---
+++ supports/data_for_benchmark.py
@@ -0,0 +1,220 @@
+# mmlu = [
+#     ["MMLU", 30.85, "bigscience\u002fbloom"],
+#     ["MMLU", 70.67, "jondurbin\u002fairoboros-l2-70b-gpt4-m2.0"],
+#     ["MMLU", 70.7, "ICBU-NPU\u002fFashionGPT-70B-V1"],
+#     ["MMLU", 70.99, "ICBU-NPU\u002fFashionGPT-70B-V1.1"],
+#     ["MMLU", 78.13, "bhenrym14\u002fplatypus-yi-34b"],
+#     ["MMLU", 78.46, "kyujinpy\u002fPlatYi-34B-LoRA"],
+#     ["MMLU", 78.78, "kyujinpy\u002fPlatYi-34B-Llama-Q"],
+#     ["MMLU", 85.6, "CausalLM\u002f34b-beta"],
+# ]
+#
+# mmlu_time = [
+#     "2023-08-25T13:05:50+00:00",
+#     "2023-09-09T10:52:17+00:00",
+#     "2023-10-16T13:19:55+00:00",
+#     "2023-11-06T10:31:15+00:00",
+#     "2023-11-15T16:49:09+00:00",
+#     "2023-12-03T12:09:22+00:00",
+#     "2023-12-06T11:25:55+00:00",
+#     "2024-02-09T16:11:28+00:00",
+# ]
+#
+# gsm8k = [
+#     ["GSM8K", 6.9, "bigscience\u002fbloom"],
+#     ["GSM8K", 35.33, "OpenLemur\u002flemur-70b-chat-v1"],
+#     ["GSM8K", 45.94, "tiiuae\u002ffalcon-180B"],
+#     ["GSM8K", 56.41, "abhishek\u002fzephyr-beta-math"],
+#     ["GSM8K", 59.82, "bhenrym14\u002fplatypus-yi-34b"],
+#     ["GSM8K", 60.8, "chargoddard\u002fYi-34B-Llama"],
+#     ["GSM8K", 62.7, "CausalLM\u002f14B-DPO-alpha"],
+#     ["GSM8K", 63.68, "deepseek-ai\u002fdeepseek-llm-67b-chat"],
+#     ["GSM8K", 68.84, "meta-math\u002fMetaMath-Mistral-7B"],
+#     ["GSM8K", 71.49, "Q-bert\u002fMetaMath-Cybertron-Starling"],
+#     ["GSM8K", 72.18, "SUSTech\u002fSUS-Chat-34B"],
+#     ["GSM8K", 73.16, "Toten5\u002fMarcoroni-neural-chat-7B-v2"],
+#     ["GSM8K", 73.31, "samir-fama\u002fFernandoGPT-v1"],
+#     ["GSM8K", 76.27, "moreh\u002fMoMo-72B-lora-1.8.4-DPO"],
+#     ["GSM8K", 76.8, "moreh\u002fMoMo-72B-lora-1.8.6-DPO"],
+#     ["GSM8K", 78.62, "moreh\u002fMoMo-72B-lora-1.8.7-DPO"],
+#     ["GSM8K", 78.7, "abacusai\u002fSmaug-72B-v0.1"],
+#     ["GSM8K", 82.03, "mistralai\u002fMixtral-8x22B-Instruct-v0.1"],
+#     ["GSM8K", 85.44, "meta-llama\u002fMeta-Llama-3-70B-Instruct"],
+#     ["GSM8K", 86.05, "MaziyarPanahi\u002fLlama-3-70B-Instruct-DPO-v0.1"],
+#     ["GSM8K", 86.28, "tenyx\u002fLlama3-TenyxChat-70B"],
+#     ["GSM8K", 88.25, "MaziyarPanahi\u002fLlama-3-70B-Instruct-DPO-v0.2"],
+# ]
+#
+# gsm8k_time = [
+#     "2023-08-25T13:05:50+00:00",
+#     "2023-09-09T10:52:17+00:00",
+#     "2023-09-15T11:25:37.918274+00:00",
+#     "2023-11-14T13:30:22+00:00",
+#     "2023-11-15T16:49:09+00:00",
+#     "2023-11-17T13:40:31+00:00",
+#     "2023-11-19T20:55:46+00:00",
+#     "2023-12-01T07:45:30+00:00",
+#     "2023-12-03T15:31:15+00:00",
+#     "2023-12-05T20:10:40+00:00",
+#     "2023-12-09T04:51:31+00:00",
+#     "2023-12-12T16:18:51+00:00",
+#     "2023-12-31T09:50:37+00:00",
+#     "2024-01-12T06:33:27+00:00",
+#     "2024-01-16T11:52:34+00:00",
+#     "2024-01-22T00:16:35+00:00",
+#     "2024-02-03T18:49:25+00:00",
+#     "2024-04-17T15:30:22+00:00",
+#     "2024-04-18T17:05:16+00:00",
+#     "2024-04-26T19:35:47+00:00",
+#     "2024-04-27T17:40:55+00:00",
+#     "2024-05-01T17:04:08+00:00",
+# ]
+
+
+# source = "https://huggingface.co/spaces/open-llm-leaderboard/blog"
+
+# gsm8k = []
+#
+# mmlu = [
+#     ["o1", 91.8, "2024-12"],
+#     ["gpt-4o", 88.7, "2024-05"],
+#     ["gpt-4", 86.4, "2023-04"],
+#     ["gpt-3.5", 67.5, "2022-05"],
+#     ["gpt-3", 43.9, "2020-05"],
+#     ["roberta", 27.9, "2019-07"],
+# ]
+#
+# gpqa = [
+#     ["GPT 4o (2024-05-13)", 50.3, "2024-05-13"],
+#     ["gpt-3.5-turbo-0125", 29.3, "2023-01-25"],
+#     ["o1", 73.0, "2024-12-17"],
+#     ["Gemini 2.5 Pro Exp", 80.3, "2025-03-25"],
+# ]
+#
+# aime = [
+#     ["o3-mini", 86.5, "2025-01-31"],
+#     ["deepseek-r1", 77.3, "2025-01-20"],
+#     ["GPT 4o (2024-08-06)", 14.0, "2024-08-06"],
+# ]
+#
+# swe = [
+#     ["W&B Programmer O1 crosscheck5", 64.60, "2025-01-17"],
+#     ["OpenHands + CodeAct v2.1 (claude-3-5-sonnet-20241022)", 53.00, "2024-10-29"],
+#     ["RAG + Claude 3 Opus", 7.00, "2024-04-02"],
+#     ["Solver (2024-09-12)", 45.40, "2024-09-24"],
+# ]
+#
+# hle = [["deepseek-r1", 9.4, "2025-01-24"], ["OpenAI deep research", 26.6, "2025-02-02"]]
+#
+# arc_agi = [
+#     ["o3", 75.7, "2024-12-20"],
+#     ["ARChitects", 53.5, "2024-12-6"],
+#     ["unknown", 34.3, "2024-05-30"],
+# ]
+#
+# mmlu_pro = [
+#     ["GPT 4o (2024-08-06)", 74.1, "2024-08-06"],
+#     ["o1", 83.5, "2024-12-17"],
+#     ["Gemini 2.5 Pro Exp", 84.1, "2025-03-25"],
+# ]
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+import seaborn as sns
+import matplotlib.dates as mdates
+
+
+
+# Define the datasets
+datasets = {
+    "GSM8K": [
+        # ["deepseek-v3", 89.3, "2024-12"],
+        ["Claude 3.5 Sonnet", 96.4, "2024-06"],
+        ["gpt-4", 92.0, "2023-05"],
+        ["minerva", 78.5, "2022-06"],
+        ["palm", 58, "2022-04"],
+    ],
+    "MMLU": [
+        ["o1", 91.8, "2024-12"],
+        ["gpt-4o", 88.7, "2024-05"],
+        ["gpt-4", 86.4, "2023-04"],
+        ["gpt-3.5", 67.5, "2022-05"],
+        # ["gpt-3", 43.9, "2020-05"],
+        # ["roberta", 27.9, "2019-07"],
+    ],
+    "MMLU-Pro": [
+        ["GPT 4o (2024-08-06)", 74.1, "2024-08-06"],
+        ["o1", 83.5, "2024-12-17"],
+        ["Gemini 2.5 Pro Exp", 84.1, "2025-03-25"],
+    ],
+    "GPQA": [
+        ["GPT 4o (2024-05-13)", 50.3, "2024-05-13"],
+        ["gpt-3.5-turbo-0125", 29.3, "2023-01-25"],
+        ["o1", 73.0, "2024-12-17"],
+        ["Gemini 2.5 Pro Exp", 80.3, "2025-03-25"],
+    ],
+    "AIME": [
+        ["o3-mini", 86.5, "2025-01-31"],
+        ["deepseek-r1", 77.3, "2025-01-20"],
+        ["GPT 4o (2024-08-06)", 14.0, "2024-08-06"],
+    ],
+    "SWE-Bench": [
+        ["W&B Programmer O1 crosscheck5", 64.60, "2025-01-17"],
+        ["OpenHands + CodeAct v2.1 (claude-3-5-sonnet-20241022)", 53.00, "2024-10-29"],
+        ["RAG + Claude 3 Opus", 7.00, "2024-04-02"],
+        ["Solver (2024-09-12)", 45.40, "2024-09-24"],
+    ],
+    "Human Last Exam": [
+        ["deepseek-r1", 9.4, "2025-01-24"],
+        ["OpenAI deep research", 26.6, "2025-02-02"],
+    ],
+    "ARC-AGI": [
+        ["o3", 75.7, "2024-12-20"],
+        ["ARChitects", 53.5, "2024-12-6"],
+        ["unknown", 34.3, "2024-05-30"],
+    ],
+
+}
+
+color_palette = "colorblind"
+color_palette = sns.color_palette(color_palette, n_colors=len(datasets.keys()))
+# Plotting each dataset
+plt.figure(figsize=(14, 7))
+for i, (name, data) in enumerate(datasets.items()):
+    df = pd.DataFrame(data, columns=["model", "score", "date"])
+    df["date"] = pd.to_datetime(df["date"], errors="coerce")
+    df = df.sort_values("date")
+    plt.plot(
+        df["date"],
+        df["score"],
+        marker="o",
+        linestyle="-",
+        linewidth=5,
+        markersize=10,
+        label=name,
+        color=color_palette[i],
+    )
+
+plt.ylim(0, 100)
+# plt.xlabel("Date", fontsize=24)
+plt.ylabel("Performance", fontsize=32)
+# plt.title("Model Performance Over Time", fontsize=24, weight="bold")
+plt.legend(fontsize=24, ncol=2, loc="lower left")
+plt.grid(True, linestyle="--", alpha=0.6)
+plt.xticks(fontsize=32)
+plt.yticks(fontsize=32)
+
+plt.gca().xaxis.set_major_locator(mdates.YearLocator())
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+# plt.xticks(fontsize=11, weight='bold')
+
+plt.tight_layout()
+
+plt.savefig(
+    "./supports/benchmark_over_time.pdf",
+    bbox_inches="tight",
+    pad_inches=0.0,
+)
+plt.show()
